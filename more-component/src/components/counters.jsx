a32e4faf5f4c05b7f0343c6b34e22849c847710d
Haider Daij <haiderduaij@gmail.com>
2022-06-23 21:13:00 +0300

    Update

--- more-component/src/components/counters.jsx
+++ more-component/src/components/counters.jsx
@@ -3,21 +3,46 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    conters: [
+    counters: [
       { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
   };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
-        {this.state.conters.map((counter) => (
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counter={counter}
           ></Counter>
         ))}
       </div>
